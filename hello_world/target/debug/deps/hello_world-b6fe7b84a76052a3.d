731cd30de0e99f2a28162f6ead1e4485bb648a7d
ion0658 <y0a6s5u8soy@gmail.com>
2022-09-27 22:41:57 +0900

    create hello world

---
+++ hello_world/target/debug/deps/hello_world-b6fe7b84a76052a3.d
@@ -0,0 +1,5 @@
+/home/ion/src/rust_learning/hello_world/target/debug/deps/hello_world-b6fe7b84a76052a3: src/main.rs
+
+/home/ion/src/rust_learning/hello_world/target/debug/deps/hello_world-b6fe7b84a76052a3.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/ion/src/rust_learning/hello_world/target/debug/hello_world: /home/ion/src/rust_learning/hello_world/src/main.rs
